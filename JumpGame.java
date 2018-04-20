a578142c75449a750b97258777282f6c27bcafa8
Yuanye Xia <vigoose@gmail.com>
2018-04-20 11:38:04 -0500

    Create JumpGame.java

---
+++ JumpGame.java
@@ -0,0 +1,10 @@
+class JumpGame {
+    public boolean canJump(int[] nums) {
+        int max = 0;
+        for (int i = 0; i<=nums.length-1; i++){
+            if (i > max) return false;
+            max = Math.max(max, nums[i] + i);
+        }
+        return true;
+    }
+}
